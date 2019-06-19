66631154d18e1a16b3dd03b522e30efbc889395b
Tobias Jacobson <44820966+TobiasJacobson@users.noreply.github.com>
2019-06-19 12:16:52 -0700

    Three functions: Path, contour, mesh

--- pyScripts.py
+++ pyScripts.py
@@ -3,10 +3,10 @@
     # Pass list of radii? In case of non-uniform vessels
     # Contour type? How to take in levelSet, splinePoly, or Polygon selections?
 
+# Application: Only cylindrical models
 
-
-# def of function for path making, lofting, and meshing
-def pathMaker(pointsList, pathName, contourName, radius):
+# Function to create path and make cylindrical control points
+def makePath(pointsList, pathName, contourName, radius):
     # Shortcut for function call Path.pyPath() needed when calling SimVascular functions
     p = Path.pyPath()
 
@@ -14,7 +14,7 @@ def pathMaker(pointsList, pathName, contourName, radius):
     p.NewObject(pathName)
     print('Path name: ' + pathName)
 
-    # Adding each point from pointsList to current path
+    # Adding each point from pointsList to created path
     for pathPoint in pointsList:
         p.AddPoint(pathPoint)
 
@@ -25,7 +25,7 @@ def pathMaker(pointsList, pathName, contourName, radius):
     GUI.ImportPathFromRepos(pathName)
     GUI.ImportPathFromRepos(pathName,'Paths')
 
-    # Initializing variables and creating contour, default to circle
+    # Initializing variables and creating segmentations, default to circle
     Contour.SetContourKernel('Circle')
     index = 0
     pointsLength = len(pointsList)
@@ -34,40 +34,26 @@ def pathMaker(pointsList, pathName, contourName, radius):
     #  Shortcut for function call Contour.pyContour() needed when calling SimVascular functions
     c = Contour.pyContour()
 
-    # Creating contours at each segment for pointsList
+    # Creating 2 control segmentations for caps of the cylinder
     c.NewObject(contourNameList[0], pathName, 0)
     c.SetCtrlPtsByRadius(pointsList[0], radius+1)
     c.Create()
     c.GetPolyData('1ctp')
 
-    numTwo = p.GetPathPtsNum() # index at end of points list
+    numTwo = p.GetPathPtsNum() # index at end of pointsList
     c2 = Contour.pyContour()
     c2.NewObject(contourNameList[1], pathName, numTwo-1)
     c2.SetCtrlPtsByRadius(pointsList[1], radius+1)
     c2.Create()
     c2.GetPolyData('2ctp')
 
-    ### Attempt at creating systematic list of names (cct0, cct1, cct2, etc..) for individual points along path based on  pointsList length
-    ### string = 'cct'
-    ### while index < (pointsLength):
-    ###     string += str(index)
-    ###     contourNameList.append(string)
-    ###     print(contourNameList[index])
-    ###     index += 1
-
-    ### create n number of objects based on pointsList length, adding each contour to repository
-    ### index = 0
-    ### while index < (pointsLength-1):
-    ###     c.NewObject(contourNameList[index], pathName, index)
-    ###     c.SetCtrlPtsByRadius(pointsList[index], radius)
-    ###     c.Create()
-    ###     index += 1
-
     # Importing contours from repository to 'Segmentations' tab in GUI
     GUI.ImportContoursFromRepos(contourName, contourNameList, pathName, 'Segmentations')
-    print('Finished creating contours')
 
-    # Gathering data to loft solid
+    return
+
+def makeContour(pointsList, pathName, contourName, radius):
+    # Creating data to loft solid
     numSegs = 60
     Geom.SampleLoop('1ctp', numSegs, '1ctps')
     Geom.SampleLoop('2ctp', numSegs, '2ctps')
@@ -77,7 +63,7 @@ def pathMaker(pointsList, pathName, contourName, radius):
     Geom.AlignProfile('1ctps', '2ctps', 'ct2psa', 0)
 
     # Declaring needed variables for lofting
-    srcList = ['1ctps', '2ctps'] #'ct3psa']
+    srcList = ['1ctps', '2ctps']
     objName ='loft'
     numSegsAlongLength = 12
     numPtsInLinearSampleAlongLength = 240 # Referenced elsewhere? In LoftSolid function? No other mention in scripting
@@ -88,27 +74,27 @@ def pathMaker(pointsList, pathName, contourName, radius):
 
     # Lofting solid using created values
     Geom.LoftSolid(srcList, objName, numSegs, numSegsAlongLength, numLinearSegsAlongLength, numModes, useFFT, useLinearSampleAlongLength)
+
     # Importing PolyData from solid to repository
     GUI.ImportPolyDataFromRepos('loft')
 
     # Adding caps to model
-    VMTKUtils.Cap_with_ids('loft','cap',0,0)
-
+    VMTKUtils.Cap_with_ids('loft','fullLoft',0,0)
 
     # Function shortcut for calling built-in functions
     s1 = Solid.pySolidModel()
 
-    #
+    # Creating model
     Solid.SetKernel('PolyData')
     s1.NewObject('cyl')
-    s1.SetVtkPolyData('cap')
+    s1.SetVtkPolyData('fullLoft')
     s1.GetBoundaryFaces(90)
     print("Creating model: \nFaceID found: " + str(s1.GetFaceIds()))
     s1.WriteNative(os.getcwd() + "/cylinder.vtp")
-    GUI.ImportPolyDataFromRepos('cap')
-    print('Caps added')
+    GUI.ImportPolyDataFromRepos('fullLoft')
+    print('Caps added to fullLoft')
 
-
+def makeMesh():
     # Meshing object
     MeshObject.SetKernel('TetGen')
     msh = MeshObject.pyMeshObject()
@@ -126,21 +112,28 @@ def pathMaker(pointsList, pathName, contourName, radius):
     msh.GetUnstructuredGrid('ug')
     Repository.WriteVtkUnstructuredGrid("ug","ascii",fileName)
     GUI.ImportUnstructedGridFromRepos('ug')
+    print('Mesh generated')
 
-    return
 
 # Must import to use/call simVascular executables
 from sv import *
 import os
 
 # Moving from root directory
-os.chdir('/Users/tobiasjacobson/Documents/Atom/solidtest')
-print('Current directory: ' + print(os.getcwd()))
+os.chdir('/Users/tobiasjacobson/Documents/Atom')
+print('Current directory: ' + os.getcwd())
 
-# Cleaning repository
-objs = Repository.List()
-for name in objs:
-    Repository.Delete(name)
+# # Cleaning repository
+# objs = Repository.List()
+# for name in objs:
+#     Repository.Delete(name)
 
-# Calling function pathMaker
-pathMaker(([[0.0,0.0,0.0],[10.0,10.0,10.0]]), 'path1', 'segment1',1.0)
+# Declaring list of points to be used for path
+listOfPoints = [[0.0,0.0,0.0],[10.0,10.0,10.0]]
+
+# Creating path using makePath() function
+makePath(listOfPoints, 'path1', 'segment1', 1.0)
+# Creating contour using makeContour() function
+makeContour(listOfPoints, 'path1', 'segment1', 1.0 )
+# Creating mesh using makeMesh() function
+makeMesh()
