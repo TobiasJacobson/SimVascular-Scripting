724e4808172df7ccb695739bccdb22be86e56a6c
Tobias Jacobson <44820966+TobiasJacobson@users.noreply.github.com>
2019-06-26 16:58:37 -0700

    General clean up

--- alteringStenosis.py
+++ alteringStenosis.py
@@ -24,7 +24,7 @@ def alteringStenosis(fileName, percentage, contourGroup):
 
     # Array/Matrix to store points
     pointsData = []
-    data =[]
+    data =[] # Not really used besides in this section...
 
     # Reading in points, making note of control vs contour points
     for iteration in inFile:
@@ -39,9 +39,9 @@ def alteringStenosis(fileName, percentage, contourGroup):
         else:
             data.append(re.findall('"([^"]*)"', iteration)) # ^ signifies start of string, * RE matches 0 or more (ab* will match 'a','ab' or 'abn'
                                                             # where n is n number of b's following), [] indicates a set of special characters
-    ct = int(data[-1][0])
+    ct = int(data[-1][0]) # Hmmm...
 
-    # Takes actual integers from segment to alter and adds to pointsData
+    # Takes the actual integers from segment to alter and copies them to pointsData
     count = 0
     for iteration in inFile:
         if "</contour_points>" in iteration:
@@ -52,57 +52,55 @@ def alteringStenosis(fileName, percentage, contourGroup):
             else:
                 stringLine = iteration
                 pointsData.append(re.findall('"([^"]*)"', stringLine))
-                # outFile.write(iteration)
+                # outFile.write(iteration) # This wrote original contour group 2 data to outfle...
 
     #########################################################################################################################
 
 
-
-    ################################## Creating matrix cVdataTranspose, i.e main matrix ##################################
+    ################################## Creating matrix called cVdataTranspose, i.e main matrix ##################################
     # List of ones to be appended to pointsData matrix
     onesArr = numpy.ones(43)
-    # Converting pointsData to float, removing first column b/c they only store indicies therefore aren't needed
+    # Converting pointsData to type: float, removing first column b/c it only contains indicies therefore isn't needed needed
     cVdata = numpy.array(pointsData)
     cVdata = cVdata.astype(numpy.float)
     cVdata = cVdata[:,1:]
     # Appending onesArr to pointsData
     cVdata = numpy.concatenate((cVdata,onesArr[:,None]), axis=1)
     # cVdata = numpy.c_[cVdata, onesArr] # Another way to append onesArr
-    # print(cVdata) # Check if its been added
     # Transpose data for matrix multiplication
     cVdataTranspose = numpy.transpose(cVdata)
-    # print cVdataTranspose # Check if its been transposed correctly
-    ######################################################################################################################
-
+    ############################################################################################################################
 
 
     ################################## Creating overal matrix of scalar, translation, and inverse translation ##################################
     # Hard coded center for now
     centerData = [-1.90810359169811, 10.874778040444664, 20.961486548177369, 1]
     # Setting factor based on users input
-    factor = math.sqrt(percentage/100.0)
+    factor = math.sqrt(abs(percentage-100)/100.0) # Without abs(x-100) stenosis goes as 5 in mine = 95 applied, 40 in mine = 60 applied
+
     # Creating Scalar Matrix (with scalar as percent stenosis given)
     scalarMatrix = [[factor, 0, 0, 0], [0, factor, 0, 0], [0, 0, factor, 0], [0, 0, 0, 1]]
+
     # Creating Translation Matrix
     translationMatrix = [[1, 0,0, 1.90810359169811], [0, 1, 0, -10.874778040444664], [0, 0, 1, -20.961486548177369], [0, 0, 0, 1]]
+
     # Creating Inverse Translation matrix
     invTranslationMatrix = [[1, 0,0, -1.90810359169811], [0, 1, 0, 10.874778040444664], [0, 0, 1, 20.961486548177369], [0, 0, 0, 1]]
+
     # Overall Matrix created
     matrixS = numpy.matmul(invTranslationMatrix, scalarMatrix)
     matrixMain = numpy.matmul(matrixS, translationMatrix)
-    # import pdb; pdb.set_trace()
-    # print matrixMain
+    # import pdb; pdb.set_trace() # Needed for debugging
     ############################################################################################################################################
 
-    # Matrix multiplication of cVdataTranspose and dataMatrix (43x4 matrix and a 4x4 matrix leaves a 43sx4) # Note: have to left  multiply with dataMatrix
+    # Matrix multiplication of cVdataTranspose and dataMatrix (43x4 matrix and a 4x4 matrix leaves a 43x4) # Note: have to left multiply with dataMatrix
     newPointsData = numpy.matmul(matrixMain, cVdataTranspose)
     newPointsData = newPointsData[:-1,:] # Removes ones from bottom of matrix
-    # print newPointsData
 
+    # Transposed scaled data back to original form
     newDataTpose = numpy.transpose(newPointsData)
-    # print newDataTpose
 
-    # Finish writing to file
+    # Adding scaled data points to the outFile
     outFile.write('            <control_points>\n')
     for i in xrange(ct+1):
         dl = newDataTpose[i,:]
@@ -117,9 +115,11 @@ def alteringStenosis(fileName, percentage, contourGroup):
         outFile.write('                '+fStr)
     outFile.write('            </contour_points>\n')
 
+    # Finish writing rest of inFile to outFile
     for line in inFile:
        outFile.write(line)
 
+    # Final actions
     print("Created.")
     inFile.close()
     outFile.close()
@@ -142,10 +142,10 @@ import re
 import math
 import os.path
 
-# Initializing data for function call
-fileInput = raw_input("Enter the name of the file to be read from: ")
-contourInput = raw_input("Enter the number of the contour you'd like to change: ")
-percentInput = raw_input("What percent stenosis are you applying: ") # Weird b/c 25 in mine = 75 applied, 75 in mine = 25 applied
+# Initializing for function call
+fileInput = raw_input("Enter the name of the file to be read from: \n")
+contourInput = raw_input("Enter the number of the contour you'd like to change: \n")
+percentInput = raw_input("What percent stenosis are you applying: \n")
 
 # Function call
 alteringStenosis(fileInput, float(percentInput), contourInput)
