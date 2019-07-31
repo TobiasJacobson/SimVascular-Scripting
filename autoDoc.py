cc77917bd16fd98e659f0761034df4c4913765cb
Tobias Jacobson <44820966+TobiasJacobson@users.noreply.github.com>
2019-07-31 11:40:55 -0700

    Function to extract doc-strings from given python functions

--- autoDoc.py
+++ autoDoc.py
@@ -1,9 +1,9 @@
 # Objective: Write a function to extract docstrings from a given python function and output them in a new pdf file
 
 def aDoc(ListFuncNames):
-    f= open("docStrings.txt","w+")
-    f.write('Here is a compilation of the doc-strings from the given list of functions. \n\n')
-
+    f = open("docStrings.txt","w+") # Creates new file
+    f.write('Here is a compilation of the doc-strings from the given list of functions. \n\n') # Adding first line to output file
+    # Loop through each function, extacting the docstrings and adding them to the output file 
     index = 0
     for item in ListFuncNames:
         string = item.__doc__
